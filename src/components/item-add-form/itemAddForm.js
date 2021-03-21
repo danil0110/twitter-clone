3e9d5b6ba2077b13a052817ec9dcdf82c94d6e46
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-03-21 20:18:18 +0200

    Add form is working now

--- src/components/item-add-form/itemAddForm.js
+++ src/components/item-add-form/itemAddForm.js
@@ -1,14 +1,43 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 
 import './itemAddForm.css';
 
-const ItemAddForm = ({onAdd}) => {
-    return (
-        <div className="bottom-panel d-flex">
-            <input type="text" placeholder="О чем вы сейчас думаете?" className="form-control new-post-label" />
-            <button type="submit" className="btn btn-outline-secondary" onClick={() => onAdd('Hello')}>Добавить</button>
-        </div>
-    )
-}
+export default class ItemAddForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        };
+        this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
 
-export default ItemAddForm;
+    onValueChange(e) {
+        this.setState({ text: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        this.props.onAdd(this.state.text);
+        this.setState({
+            text: ''
+        });
+    }
+
+    render() {
+        return (
+            <form
+                className="bottom-panel d-flex"
+                onSubmit={this.onSubmit}>
+                <input
+                    type="text"
+                    placeholder="О чем вы сейчас думаете?"
+                    className="form-control new-post-label"
+                    onChange={this.onValueChange}
+                    value={this.state.text} />
+                <button type="submit" className="btn btn-outline-secondary">Добавить</button>
+            </form>
+        )
+    }
+}
